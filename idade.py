66efb96db76cd8d6eee7cdd99557b5b56174b7e2
lg0mexs25 <lauragomes.2825@gmail.com>
2025-06-10 11:38:42 -0300

    Add files via upload

---
+++ idade.py
@@ -0,0 +1,7 @@
+nome = input("qual seu noem?")
+idade = int( input( "qual é sua idade"))
+
+if idade >=24:
+    print( "maior de idade ")
+else:
+     print( "menor de idade")
---
+++ nota.py
@@ -0,0 +1,11 @@
+primeira_nota = float(input ( "digite sua primeira nota"))
+segunda_nota = float(input ( "digite sua segunda nota"))
+terceira_nota = float(input ( "digite sua terceira nota"))
+soma = ( primeira_nota + segunda_nota + terceira_nota) / 3
+
+if soma >=7:
+   print("albert einstein tá como")
+elif soma >=4:
+    print(" melhore")
+else:
+   print( "burrão")
